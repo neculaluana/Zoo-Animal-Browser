6b2a4f106f99108d4c45c3638c022457740820e3
Necula Luana <neculaluana@users.noreply.github.com>
2024-04-30 04:04:33 +0300

    implemented animal detail fragment logic and layout

--- app/src/main/java/com/example/zooanimalbrowser/AnimalDetailFragment.kt
+++ app/src/main/java/com/example/zooanimalbrowser/AnimalDetailFragment.kt
@@ -1,4 +1,30 @@
 package com.example.zooanimalbrowser
 
-class AnimalDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class AnimalDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.animal_detail_fragment, container, false)
+
+        val nameTextView: TextView = view.findViewById(R.id.name)
+        val continentTextView: TextView = view.findViewById(R.id.continent)
+        val bgColor = arguments?.getInt("bgColor") ?: android.R.color.transparent
+        val textColor = arguments?.getInt("textColor") ?: android.R.color.black
+        nameTextView.text = arguments?.getString("name")
+        continentTextView.text = arguments?.getString("continent")
+        view.setBackgroundColor(bgColor)
+        nameTextView.setTextColor(textColor)
+        continentTextView.setTextColor(textColor)
+
+        return view
+    }
 }
